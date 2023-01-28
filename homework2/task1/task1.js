aab127baa1014849a9dd2cf48176218f504f7192
magrega <proshanov@yandex.ru>
2023-01-28 11:30:52 +0300

    did first and second tasks

--- homework2/task1/task1.js
+++ homework2/task1/task1.js
@@ -1,4 +1,3 @@
-
 // const COUNTER = document.querySelector('.counter');
 // let count = 0;
 
@@ -26,45 +25,40 @@
 // методы функции - мое решение
 function dosmth(counter = 0) {
 
-    this.plus = function (num) {
-        counter = counter + num;
-    };
-
-    this.minus = function (num) {
-        counter = counter - num;
-    };
-
-    this.reset = function () {
-        counter = 0;
-    };
-
-
-    this.plus(5);
-    this.minus(2);
-    
-
-    return counter;
+    return {
+        currentValue: counter,
+        plus(num = 1) {
+            return this.currentValue += num;
+        },
+        minus(num = 1) {
+            return this.currentValue -= num;
+        },
+        reset() {
+            return this.currentValue = counter;
+        }
+    }
 };
 
-console.log(dosmth(0));
+console.log(dosmth(0).plus(5));
 
 // методы функции - решение от Егора
-// function dosmth(counter = 0) {
+// function createCounter(initialValue = 0) {
 
 //     return {
-//         currentValue: counter,
-//         plus(num = 1) {
-//             console.log(this.plus instanceof dosmth);
-//             return this.currentValue += num;
+//         currentValue: initialValue,
+//         showValue() {
+//             return this.currentValue;
 //         },
-//         minus(num = 1) {
-//             return this.currentValue -= num;
+//         increment(number = 1) {
+//             return this.currentValue += number;
 //         },
-//         reset() {
-//             return this.currentValue = counter;
+//         decrement(number = 1) {
+//             return this.currentValue -= number;
+//         },
+//         discard() {
+//             return this.currentValue = initialValue;
 //         }
 //     }
 // };
 
-// console.log(dosmth(2).plus(5));
-
+// console.log(createCounter(0).increment(5).decrement(3));
